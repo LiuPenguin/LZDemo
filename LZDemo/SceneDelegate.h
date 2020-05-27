39288c095fe9cf8dedd32f4d52a29feea9e4203e
Penguin Liu <penguin_liu@163.com>
2020-05-27 20:16:58 +0800

    Initial Commit

---
+++ LZDemo/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  LZDemo
+//
+//  Created by penguin on 2020/5/27.
+//  Copyright © 2020 penguin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ LZDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LZDemo
+//
+//  Created by penguin on 2020/5/27.
+//  Copyright © 2020 penguin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
